f49456891215a3e60a9b1a95aac7098311f823b6
md-amir-gauhar <gauharamir15@gmail.com>
2025-06-18 02:07:38 +0530

    refactor: Update Next.js configuration to use TypeScript and simplify remote image patterns

--- next.config.ts
+++ next.config.ts
@@ -1,24 +1,12 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
   images: {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "picsum.photos",
-        port: "",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "i.pravatar.cc",
-        port: "",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "storage.googleapis.com",
-        port: "",
-        pathname: "/**",
+        hostname: "**", // Accept images from any host
       },
     ],
   },
